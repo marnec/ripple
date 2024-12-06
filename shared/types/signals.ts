2338e7f7798d76b372f5a1c9f8e1302c406131bd
marnec <marnec@ryzenbeast.localdomain>
2024-12-06 01:25:24 +0100

    remove unused imports

--- shared/types/signals.ts
+++
@@ -1,1 +0,0 @@
-import { Infer } from "convex/values";
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "next-themes";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
+import { ChannelVideoCall } from "./components/Channel/ChannelCall.tsx";
 import { ChatLayout } from "./components/Chat/ChatLayout.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { WorkspaceDetails } from "./components/Workspace/WorkspaceDetails.tsx";
@@ -12,7 +12,6 @@ import { WorkspaceSettings } from "./components/Workspace/WorkspaceSettings.tsx"
 import "./index.css";
 import { InviteAcceptPage } from "./pages/InviteAcceptPage.tsx";
 import { UserProfilePage } from "./pages/UserProfilePage.tsx";
-import { ChannelVideoCall } from "./components/Channel/ChannelCall.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
