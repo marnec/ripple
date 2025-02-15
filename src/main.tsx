7a33687b336420c582c37b4fca80a77581232a10
marnec <marnec@ryzenbeast.localdomain>
2025-02-15 12:29:37 +0100

    add a meta tag to alter behaviour of the viewport

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,7 @@ export const useQueryWithStatus = makeUseQueryWithStatus(useQueries);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content" />
     <ThemeProvider attribute="class">
       <Toaster />
       <ConvexAuthProvider client={convex}>
