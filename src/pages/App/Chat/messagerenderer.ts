03572841fc9da50ade8e2bfc8199a8791cc9c7e3
Marco <rvrmarco@gmail.com>
2025-06-08 16:25:11 +0200

    remove unused messagerenderer

--- src/pages/App/Chat/messagerenderer.ts
+++
@@ -1,8 +0,0 @@
-import { useCreateBlockNote } from "@blocknote/react";
-import { useMemo } from "react";
-
-export const useCreateMessageRenderer = () => {
-  return useMemo(() => {
-    return useCreateBlockNote();
-  }, []);
-};
