1493d3f85ad8ffe85c726464bc9e232e12596fe7
marnec <rvrmarco@gmail.com>
2025-07-09 19:38:39 +0200

    make diagram block resizeable

--- src/pages/App/Document/CustomBlocks/DiagramBlock.tsx
+++ src/pages/App/Document/CustomBlocks/DiagramBlock.tsx
@@ -1,9 +1,12 @@
-import { createReactBlockSpec } from "@blocknote/react";
+import {
+  createReactBlockSpec,
+  ReactCustomBlockRenderProps,
+} from "@blocknote/react";
 import { useQuery } from "convex/react";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
 import { exportToSvg } from "@excalidraw/excalidraw";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   ExcalidrawElement,
   NonDeleted,
@@ -13,9 +16,15 @@ import { useSanitize } from "../../../../hooks/use-sanitize";
 import { Skeleton } from "../../../../components/ui/skeleton";
 import { useTheme } from "next-themes";
 import { CircleSlash } from "lucide-react";
+import { defaultProps } from "@blocknote/core";
 
-
-const DiagramView = ({ diagramId }: { diagramId: Id<"diagrams"> }) => {
+const DiagramView = ({
+  diagramId,
+  onAspectRatioChange,
+}: {
+  diagramId: Id<"diagrams">;
+  onAspectRatioChange?: (ratio: number) => void;
+}) => {
   const diagram = useQuery(api.diagrams.get, { id: diagramId });
   const [svg, setSvg] = useState<string | null>(null);
   const sanitize = useSanitize();
@@ -46,6 +55,13 @@ const DiagramView = ({ diagramId }: { diagramId: Id<"diagrams"> }) => {
           appState,
           files: {},
         }).then((svgElement: SVGSVGElement) => {
+          const svgWidth = parseFloat(svgElement.getAttribute("width") || "0");
+          const svgHeight = parseFloat(svgElement.getAttribute("height") || "0");
+          if (onAspectRatioChange && svgWidth > 0 && svgHeight > 0) {
+            onAspectRatioChange(svgHeight / svgWidth);
+          }
+          svgElement.setAttribute("width", "100%");
+          svgElement.setAttribute("height", "100%");
           // Excalidraw's export function can leave the font as "Virgil", but the web-font is "Virgil, Segoe UI Emoji"
           // We need to replace it to make sure the font is rendered correctly.
           const svgString = svgElement.outerHTML.replace(
@@ -93,9 +109,180 @@ const DiagramView = ({ diagramId }: { diagramId: Id<"diagrams"> }) => {
 
   return (
     <div
-      className="w-full flex items-center justify-center"
+      className="w-full h-full"
       dangerouslySetInnerHTML={{ __html: sanitizedSvg }}
     />
+  );
+};
+
+const ResizableDiagram = ({ block, editor }: any) => {
+  const { diagramId } = block.props;
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const resizeParamsRef = useRef<{
+    handleUsed: "l" | "r";
+    initialWidth: number;
+    initialClientX: number;
+  }>();
+
+  const [aspectRatio, setAspectRatio] = useState<number | null>(null);
+  // state to show/hide handles
+  const [showHandles, setShowHandles] = useState(false);
+
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+    if (!wrapper) return;
+
+    const windowMouseMoveHandler = (event: MouseEvent) => {
+      if (!resizeParamsRef.current) {
+        return;
+      }
+
+      let newWidth: number;
+      const resizeParams = resizeParamsRef.current;
+      const xDiff = event.clientX - resizeParams.initialClientX;
+
+      if (block.props.textAlignment === "center") {
+        if (resizeParams.handleUsed === "r") {
+          newWidth = resizeParams.initialWidth + xDiff * 2;
+        } else {
+          newWidth = resizeParams.initialWidth - xDiff * 2;
+        }
+      } else {
+        if (resizeParams.handleUsed === "r") {
+          newWidth = resizeParams.initialWidth + xDiff;
+        } else {
+          newWidth = resizeParams.initialWidth - xDiff;
+        }
+      }
+
+      const minWidth = 64;
+      const editorWidth = (editor.domElement?.firstElementChild as HTMLElement)
+        ?.clientWidth;
+
+      const finalWidth = Math.min(
+        Math.max(newWidth, minWidth),
+        editorWidth || Number.MAX_VALUE,
+      );
+      wrapper.style.width = `${finalWidth}px`;
+      if (aspectRatio) {
+        wrapper.style.height = `${finalWidth * aspectRatio}px`;
+      }
+    };
+
+    const windowMouseUpHandler = () => {
+      if (!resizeParamsRef.current) {
+        return;
+      }
+
+      resizeParamsRef.current = undefined;
+
+      editor.updateBlock(block, {
+        props: {
+          width: wrapper.clientWidth,
+        },
+      });
+    };
+
+    window.addEventListener("mousemove", windowMouseMoveHandler);
+    window.addEventListener("mouseup", windowMouseUpHandler);
+
+    return () => {
+      window.removeEventListener("mousemove", windowMouseMoveHandler);
+      window.removeEventListener("mouseup", windowMouseUpHandler);
+    };
+  }, [editor, block, aspectRatio]);
+
+  const resizeHandleMouseDownHandler = (
+    event: React.MouseEvent,
+    handle: "l" | "r",
+  ) => {
+    event.preventDefault();
+    if (!wrapperRef.current) return;
+    resizeParamsRef.current = {
+      handleUsed: handle,
+      initialWidth: wrapperRef.current.clientWidth,
+      initialClientX: event.clientX,
+    };
+  };
+
+  if (!diagramId) {
+    return (
+      <div className="p-4 border rounded-lg text-center text-gray-500">
+        <p>Please select a diagram to display.</p>
+      </div>
+    );
+  }
+
+  const resizeHandleStyle: React.CSSProperties = {
+    position: "absolute",
+    width: "8px",
+    height: "40px",
+    backgroundColor: "black",
+    borderRadius: "4px",
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'white',
+    zIndex: 10,
+  };
+
+  return (
+    <div
+      {...block.contentAttributes}
+      ref={wrapperRef}
+      className="relative"
+      style={{
+        width: block.props.width,
+        height:
+          aspectRatio && block.props.width
+            ? `${block.props.width * aspectRatio}px`
+            : "auto",
+      }}
+      onMouseEnter={() => editor.isEditable && setShowHandles(true)}
+      onMouseLeave={(event) => {
+        if (
+          (event.relatedTarget as HTMLElement)?.classList.contains(
+            "bn-resize-handle",
+          ) ||
+          resizeParamsRef.current
+        ) {
+          return;
+        }
+        setShowHandles(false);
+      }}>
+      <div className="p-4 border rounded-lg h-full">
+        <DiagramView
+          diagramId={diagramId as Id<"diagrams">}
+          onAspectRatioChange={setAspectRatio}
+        />
+      </div>
+      {showHandles && (
+        <>
+          <div
+            className="bn-resize-handle"
+            style={{
+              ...resizeHandleStyle,
+              top: "50%",
+              left: 4,
+              cursor: "ew-resize",
+              transform: "translateY(-50%)",
+            }}
+            onMouseDown={(e) => resizeHandleMouseDownHandler(e, "l")}
+          />
+          <div
+            className="bn-resize-handle"
+            style={{
+              ...resizeHandleStyle,
+              top: "50%",
+              right: 4,
+              cursor: "ew-resize",
+              transform: "translateY(-50%)",
+            }}
+            onMouseDown={(e) => resizeHandleMouseDownHandler(e, "r")}
+          />
+        </>
+      )}
+    </div>
   );
 };
 
@@ -103,28 +290,19 @@ export const DiagramBlock = createReactBlockSpec(
   {
     type: "diagram",
     propSchema: {
+      ...defaultProps,
       diagramId: {
         default: null as unknown as Id<"diagrams">,
+      },
+      width: {
+        default: 512,
       },
     },
     content: "none",
   },
   {
-    render: ({ block }) => {
-      const { diagramId } = block.props;
-
-      if (!diagramId) {
-        return (
-          <div className="p-4 border rounded-lg text-center text-gray-500">
-            <p>Please select a diagram to display.</p>
-          </div>
-        );
-      }
-      return (
-        <div className="p-4 border rounded-lg">
-          <DiagramView diagramId={diagramId as Id<"diagrams">} />
-        </div>
-      );
+    render: (props: any) => {
+      return <ResizableDiagram {...props} />;
     },
   },
 ); 
