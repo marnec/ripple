95a4bedda92944e80aa2079edc0a44aeab9cddbf
marnec <rvrmarco@gmail.com>
2026-05-02 17:20:42 +0200

    chore: bump react version

--- src/routes.tsx
+++ src/routes.tsx
@@ -300,13 +300,4 @@ export const router = createBrowserRouter(
         })),
     },
   ],
-  {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    },
-  },
 );
