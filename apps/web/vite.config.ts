ccc7ce84ada7e85577cd6aa5cb323f1149452fdf
marnec <rvrmarco@gmail.com>
2026-05-04 15:28:45 +0200

    chore: disable pwa in dev to prevent infinite reloads

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
         type: "module",
       },
     }),
